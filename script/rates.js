67775ecc0bffcb8bee2788f26cfea6b1e5db80bc
Matt Briselli <MattBriselli@gmail.com>
2017-12-03 08:09:38 -0500

    email

--- script/rates.js
+++ script/rates.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    console.log($(".bottom img"))
+    $(".bottom img").tipsy({
+        gravity: "s"
+    });
+
     $("button.submit").on("click", function() {
         $(form).ajaxSubmit({
             type:"POST",
