f78501898dda27555ab14a1c8bc19db385e25c55
Matt Briselli <MattBriselli@gmail.com>
2017-12-03 08:01:58 -0500

    tipsy

--- script/rates.js
+++ script/rates.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
-    mail();
+    $("button.submit").on("click", function() {
+        $(form).ajaxSubmit({
+            type:"POST",
+            data: $(form).serialize(),
+            url:"process.php",
+            success: function() {
+                $('#contact :input').attr('disabled', 'disabled');
+                $('#contact').fadeTo( "slow", 0.15, function() {
+                    $(this).find(':input').attr('disabled', 'disabled');
+                    $(this).find('label').css('cursor','default');
+                    $('#success').fadeIn();
+                });
+            },
+            error: function() {
+                $('#contact').fadeTo( "slow", 0.15, function() {
+                    $('#error').fadeIn();
+                });
+            }
+        });
+    });
 });
-
-function mail() {
-    
-}
