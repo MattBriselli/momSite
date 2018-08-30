c31beda04ff8782754e819c24128224c1c6ee440
Matt Briselli <MattBriselli@gmail.com>
2018-08-30 18:08:06 -0400

    working pages

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .listen('9000', () => console.log(`Listening on 9000`))
 
 app.get('/', (req, res) => {
     res.render('pages/index', {});
 });
-app.get("/stock", (req, res) => res.render('pages/stock'));
-app.get("/profile", (req, res) => res.render('pages/profile'));
-app.get("/tile", (req, res) => res.redirect('http://www.tinyurl.com/tabtiles'))
-app.get("/*", (req, res) => res.redirect('http://www.tinyurl.com/tabtiles'))
+// app.get("/stock", (req, res) => res.render('pages/stock'));
+// app.get("/profile", (req, res) => res.render('pages/profile'));
+// app.get("/tile", (req, res) => res.redirect('http://www.tinyurl.com/tabtiles'))
+// app.get("/*", (req, res) => res.redirect('http://www.tinyurl.com/tabtiles'))
