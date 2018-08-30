5874ba97e45df284661671718f528c664301b55d
Matt Briselli <MattBriselli@gmail.com>
2018-08-30 18:59:27 -0400

    correct port

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .listen('9000', () => console.log(`Listening on 9000`))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 app.get('/', (req, res) => res.render('pages/index'));
-// app.get("/index.html", (req, res) => res.redirect('/'));
 app.get("/biographies.html", (req, res) => res.render('pages/biographies'));
 app.get("/repertoire.html", (req, res) => res.render('pages/repertoire'));
 app.get("/samples.html", (req, res) => res.render('pages/samples'));
