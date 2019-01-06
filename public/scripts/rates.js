08a289914503fa2a2154f85eb56272bceed3ef8d
Matt Briselli <MattBriselli@gmail.com>
2019-01-06 14:24:08 -0800

    style sorting

--- public/scripts/rates.js
+++ public/scripts/rates.js
@@ -1,36 +1,37 @@
 $(document).ready(function() {
-    $(".bottom img").tipsy({ gravity: "s" });
+    
+    // $(".bottom img").tipsy({ gravity: "s" });
 
-    var d = new Date(),
-        dY = d.getFullYear(),
-        dM = ("0" + (d.getMonth() + 1)).slice(-2),
-        dD = ("0" + d.getDate()).slice(-2),
-        dForm = dY+"-"+dM+"-"+dD;
-    $(".form input[name='date']").val(dForm);
+    // var d = new Date(),
+    //     dY = d.getFullYear(),
+    //     dM = ("0" + (d.getMonth() + 1)).slice(-2),
+    //     dD = ("0" + d.getDate()).slice(-2),
+    //     dForm = dY+"-"+dM+"-"+dD;
+    // $(".form input[name='date']").val(dForm);
 
-    $("button.submit").on("click", function() {
-        var data = $(".bottom .form").serialize();
-        console.log(data);
+    // $("button.submit").on("click", function() {
+    //     var data = $(".bottom .form").serialize();
+    //     console.log(data);
 
-        $.ajax({
-            type: "POST",
-            data: data,
-            url: "process.php",
-            success: function() {
-                $('#contact :input').attr('disabled', 'disabled');
-                $('#contact').fadeTo( "slow", 0.15, function() {
-                    $(this).find(':input').attr('disabled', 'disabled');
-                    $(this).find('label').css('cursor','default');
-                    $('#success').fadeIn();
-                });
-                console.log('success');
-            },
-            error: function() {
-                $('#contact').fadeTo( "slow", 0.15, function() {
-                    $('#error').fadeIn();
-                });
-                console.log('error');
-            }
-        });
-    });
+    //     $.ajax({
+    //         type: "POST",
+    //         data: data,
+    //         url: "process.php",
+    //         success: function() {
+    //             $('#contact :input').attr('disabled', 'disabled');
+    //             $('#contact').fadeTo( "slow", 0.15, function() {
+    //                 $(this).find(':input').attr('disabled', 'disabled');
+    //                 $(this).find('label').css('cursor','default');
+    //                 $('#success').fadeIn();
+    //             });
+    //             console.log('success');
+    //         },
+    //         error: function() {
+    //             $('#contact').fadeTo( "slow", 0.15, function() {
+    //                 $('#error').fadeIn();
+    //             });
+    //             console.log('error');
+    //         }
+    //     });
+    // });
 });
