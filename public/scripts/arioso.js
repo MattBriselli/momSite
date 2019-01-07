90ca725798a1b75575ce352b6665db02ff0dec07
Matt Briselli <MattBriselli@gmail.com>
2019-01-06 17:29:25 -0800

    center fixing

--- public/scripts/arioso.js
+++ public/scripts/arioso.js
@@ -23,7 +23,7 @@ function heightSetter() {
         footHeight = $(".footerRow:not(.mock)").outerHeight(),
         headRow = $(".headerRow").height(),
         mobileHeadRow = $(".mobileHead").height(),
-        buffer = 55;
+        buffer = 30;
 
     var headHeight = (headRow == undefined || headRow == null || headRow == 0) 
         ? mobileHeadRow : headRow;
