f3ed0c789e6468136f32324deee674372ef41051
Matt Briselli <MattBriselli@gmail.com>
2019-03-31 17:44:37 -0700

    new

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const app = express();
+const ejsLint = require("ejs-lint");
+
+console.log(ejsLint.lint("pages/venues", {}));
 
 app.use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
